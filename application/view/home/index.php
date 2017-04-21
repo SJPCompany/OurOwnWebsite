9810989291dfdc5dde9ecec243497697e24ad4d1
Stef Bakker <stefbakker@live.nl>
2017-04-21 09:48:17 +0200

    no message

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -1,5 +1,8 @@
 <div class="container">
-    
+    <div class="slideOne">
+        <input type="checkbox" value="None" id="slideOne" name="check" checked />
+        <label for="slideOne"></label>
+    </div>
    <h1> Main page</h1>
     <p> Welcome to the SJP Company</p>
     <p> Use the menu above to hover around</p>

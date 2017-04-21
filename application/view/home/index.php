e442754cd5471f791c15ac06f5863b6235a1cb33
Justin van de laar <justin555@live.nl>
2017-04-21 09:43:39 +0200

    Added user interface

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -5,14 +5,12 @@
     <h1>Welcome to the SJP company</h1>
     <p> Please use the form below to register or log in on the site:</p>
     <br>
-    <form method="POST" action="../Website/home/doLogin">
+    <form method="POST" action="<?php echo URL; ?>home/doLogin">
         <label> username: </label> <br>
         <input type="text" name="username"> <br> <br>
         <label> password: </label> <br>
         <input type="password" name="password"> <br> <br>
         <input type="hidden" name="role" value="member">
-        <label> email: </label> <br>
-        <input type="text" name="email"> <br> <br>
         <input type="submit" name="submit" value="submit"><br><br>
         <a href="#"> Forgot password ?</a><br>
     </form>
